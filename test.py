fd03b8c06816da2446496ec7f5a701ad506d24e2
Josiah Glosson <soujournme@gmail.com>
2024-06-07 16:40:56 -0500

    Optimize test.py

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ System.out.println('Start')
 
 old_out = System.out
 System.setOut(PrintStream(OutputStream.nullOutputStream()))
+out = System.out
 for i in range(1_000_000):
-    System.out.println(i)
+    out.println(i)
 System.setOut(old_out)
 
 System.out.println('End')
