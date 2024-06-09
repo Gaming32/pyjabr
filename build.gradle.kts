ddd301f59522dfe2f16cafe39b6466bcde72d27f
Josiah Glosson <soujournme@gmail.com>
2024-06-09 08:01:23 -0500

    Migrate to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    `java-library`
+}
+
+group = "io.github.gaming32"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api("com.google.guava:guava:33.2.1-jre")
+    api("it.unimi.dsi:fastutil:8.5.13")
+    api("org.slf4j:slf4j-api:2.0.13")
+
+    compileOnly("org.jetbrains:annotations:24.1.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(22)
+    }
+}
+
+tasks.compileJava {
+    options.release = 22
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
