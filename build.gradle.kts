63a98070aa1a38c8adb025ff3b021e59d1a731f8
Josiah Glosson <soujournme@gmail.com>
2024-06-19 20:00:11 -0500

    Build sources and javadocs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     `java-library`
+    `maven-publish`
 }
 
 group = "io.github.gaming32"
@@ -27,6 +28,8 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(22)
     }
+    withSourcesJar()
+    withJavadocJar()
 }
 
 tasks.compileJava {
@@ -35,4 +38,50 @@ tasks.compileJava {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        fun maven(name: String, releases: String, snapshots: String) {
+            maven {
+                this.name = name
+                url = uri(if (version.toString().endsWith("-SNAPSHOT")) snapshots else releases)
+                credentials(PasswordCredentials::class)
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+
+        maven(
+            "gaming32",
+            "https://maven.jemnetworks.com/releases",
+            "https://maven.jemnetworks.com/snapshots"
+        )
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = project.name
+            groupId = project.group.toString()
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name = project.name
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://github.com/Gaming32/pyjabr/blob/main/LICENSE"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "Gaming32"
+                    }
+                }
+            }
+        }
+    }
 }
